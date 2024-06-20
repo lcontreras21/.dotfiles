aa80f60711736267ac6884c87096d41e365e1223
lcontreras <luiscontrerasorendain@gmail.com>
2024-06-20 18:00:54 -0400

    added room for ubuntu testing; fzf in container build script

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo docker compose -f bootstraps/docker-compose.yml up --build -d dotfiles-indirect
+
+choices=`echo "pop_os-dotfiles-direct pop_os-dotfiles-indirect ubuntu-dotfiles-indirect" | tr ' ' '\n'`
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=`printf "$choices" | fzf`
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+sudo docker compose -f bootstraps/docker-compose.yml up --build -d $selected 
