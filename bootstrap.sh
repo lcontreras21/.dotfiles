00226295bb0f13450c5d0fb95c844ddae4426159
lcontreras <luiscontrerasorendain@gmail.com>
2024-06-21 20:09:14 -0400

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 cd $HOME 
+
 FOLDER=".dotfiles"
-git clone -b ansible-izing https://github.com/lcontreras21/.dotfiles.git ${FOLDER}
+REPO="https://github.com/lcontreras21/.dotfiles.git"
+
+git clone -b ansible-izing --recurse-submodules ${REPO} ${FOLDER}
 cd ${FOLDER}
 
 set -e
 export PATH=$PATH:.local/bin
 pip install ansible --break-system-packages
 
-export PATH="$PATH:~/.local/bin"
 ansible-galaxy install -r bootstraps/requirements.yml
 
 # TODO: Use ask-become pass command on Prod or use some sort of test env
-
 # ansible-playbook bootstraps/bootstrap.yml --ask-become-pass
+
 ansible-playbook bootstraps/bootstrap.yml --extra-vars "ansible_sudo_pass=lcontreras21"
